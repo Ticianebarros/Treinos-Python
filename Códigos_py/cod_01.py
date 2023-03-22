50d0fb40e1eeaca7e246b47aa5a3080734ab565e
Ticiane Barros <110422127+Ticianebarros@users.noreply.github.com>
2023-03-22 14:52:56 -0300

    Novo

---
+++ Códigos_py/cod_01.py
@@ -0,0 +1,15 @@
+print("Hello World")
+
+nome = "Ticiane"
+print("Eu sou a " + nome)
+
+idade = 17
+print("Eu tenho " + str(idade) + " anos")
+
+local = "Aracati"
+print("Eu moro em " + local)
+
+emoji = "😀"
+print("Eu estou + emoji)
+
+print("É isso aí!, obrigada!")
